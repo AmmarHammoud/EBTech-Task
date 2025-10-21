72ea3e570b549a13d75e037c0a31339ea76f8200
Ammar Hammoud <st.ammar.hammoud@gmail.com>
2025-10-21 15:37:02 +0300

    update comment model

--- lib/models/comment_model.dart
+++ lib/models/comment_model.dart
@@ -4,4 +4,20 @@ class CommentModel {
   late String commenterAvatar;
   late String content;
   late DateTime time;
+
+  CommentModel.fromJson(Map<String, dynamic> json) {
+    id = json['comment_id'];
+    commenterName = json['commenter_name'];
+    commenterAvatar = json['commenter_avatar'];
+    content = json['content'];
+    time = json['time'];
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'commenterName': commenterName,
+    'commenterAvatar': commenterAvatar,
+    'content': content,
+    'time': time,
+  };
 }
